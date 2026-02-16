08bf2db6a2f888c38b124e45c65ac90cdf858f3e
adrien28100 <adrien94.63@gmail.com>
2026-02-16 18:08:05 +0100

    readme

--- admin/check_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
-
-//vérifie si l'utilisateur est connecté et s'il est admin
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    header("Location: ../pages/login.php?error=access_denied");
-    exit();
-}
-?>
